5652c1a5d723374aefe51c05c19e7fc53d9715fb
Astroport <58441901+Astroport@users.noreply.github.com>
2019-12-04 15:58:54 +0500

    Update Armstrong.rs
    Armstrong changes

--- Armstrong.rs
+++ Armstrong.rs
@@ -1,21 +1,39 @@
-fn main(){
-  let mut n = 153;
+use std::io;
+
+fn main()
+{
+  let mut n = String::new();
+  println!("Enter a number: ");
+  io::stdin().read_line(&mut n).unwrap();
+
+  match n.trim_right().parse::<i32>() 
+  {
+    Ok(i) => check(i),
+    Err(_) => println!("Invalid number."),
+  }
+  
+}
+
+fn check(mut n: i32)
+{
   let mut rem;
   let mut sum = 0;
-  let temp;
+  let temp = n;
 
- temp = n ;
-
-while n > 0 {
+  while n > 0 
+  {
   rem = n % 10 ;
   sum = sum + (rem * rem * rem);
   n = n / 10;
-}
+  }
 
-if temp == sum {
-  println!("{} is an Armstrong number",temp);
-}else{
+  if temp == sum 
+  {
+    println!("{} is an Armstrong number",temp);
+  }
+  else
+  {
   println!("{} is not an Armstrong number ",temp);
-}
+  }
 
 }
